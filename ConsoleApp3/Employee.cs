f5d4915a2c01dac8eb212c172de5a40fdbfafc45
Sona V. Abbasova <sonava@code.local>
2023-10-17 17:54:19 +0400

    TAmamlanmayib

---
+++ ConsoleApp3/Employee.cs
@@ -0,0 +1,26 @@
+﻿public class Employee
+{
+    public string Name { get; set; }
+    public  int Age { get; set; }
+    public double Salary { get; set; }
+
+    public Employee[] Employees = { };
+
+    public Employee(string name, int age, double salary)
+    {
+        Name = name;
+        Age = age;
+        Salary = salary;
+    }
+
+    public Employee()
+    {
+    }
+
+    public void Show()
+    {
+
+    }
+
+
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,10 @@
+﻿Employee employee1 = new Employee("Sona",20,2000);
+Employee employee2 = new Employee("Saleh",21,2000);
+Employee employee3 = new Employee("Fidan",18,800);
+Employee employee4 = new Employee("Malik",22,1000);
+Employee[] employees = new Employee[0];
+
+for(int i = 0; i <= employees.Length; i++)
+{
+
+}
